ecd0cb0199177b0380dbee8269b223cd6c17a039
Radis Lamsuneekan <radisworldclassic.gm@gmail.com>
2022-12-13 16:37:08 +0700

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,24 @@
 <template>
   <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
     <div class="container">
-      <a class="navbar-brand" href="/">
-        CE-Songkhla
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="/"> CE-Songkhla </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/admin">Admin</a>
+            <a class="nav-link active" aria-current="page" href="/admin"
+              >Admin</a
+            >
           </li>
         </ul>
         <form class="d-flex">
@@ -22,22 +30,21 @@
             <a class="btn btn-danger" href="/logout">Logout</a>
           </div>
         </form>
-
       </div>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Navbar',
+  name: "Navbar",
   data() {
     return {
       user: null,
       token: null,
-    }
+    };
   },
-})
+});
 </script>
